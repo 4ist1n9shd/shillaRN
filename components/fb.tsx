cf23b9aa33dc03642b79242a9573cb9aa55252bb
bmnbjo <bmnbjo@gmail.com>
2025-01-22 16:38:25 +0900

    테스트커밋

---
+++ components/fb.tsx
@@ -0,0 +1,17 @@
+import { View , Text, Button, FlatList} from 'react-native';
+
+
+function fb(props) {
+
+
+
+    return (
+        <View>
+            <Text>딩딩딩 </Text>
+            
+
+        </View>
+    );
+}
+
+export default fb;
