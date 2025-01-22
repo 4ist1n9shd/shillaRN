258eacec54f07380af134b6e4d0434181254b367
4ist1n9shd <prominent09@gmail.com>
2025-01-22 16:48:18 +0900

    후기게시판 연습 커밋 수훈

---
+++ components/reviewBoard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function ReviewBoard(props) {
+    return (
+        <View>
+            후기게시판입니다
+        </View>
+    );
+}
+
+export default ReviewBoard;
