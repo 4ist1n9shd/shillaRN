244c8dc5c798e3638f121cad08c66d84885dd457
4ist1n9shd <prominent09@gmail.com>
2025-01-23 14:58:18 +0900

    인덱스수정

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
-
-import fb from './components/fb';
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => fb);
+AppRegistry.registerComponent(appName, () => App);
