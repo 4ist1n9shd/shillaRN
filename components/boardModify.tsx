6dc00effc9615b2d55bd17158879b7c775fa26a4
leear613 <leear613@daum.net>
2025-01-24 15:01:18 +0900

    재커밋

--- components/boardModify.tsx
+++ components/boardModify.tsx
@@ -1,29 +1,109 @@
-import React, { useState } from 'react';
-import { Text, View, Button } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import React, {useState} from 'react';
+import { Alert, Button, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
-function boardModify({ route  , navigation}) {
+function boardDetail({ route  , navigation}) {
 
-    const {data,modifyReg} = route.params;
+    const {data, deleteReg, listModifyReg} = route.params;
 
-    const [title, setTitle] = useState(data.title)
-    const [content, setContent] = useState(data.content)
+    const [detailData, setDetailData] = useState(data);
 
-    const modifyClk = ()=>{
-        const newItem = { id : data.id, title, content }
-        modifyReg(newItem)
-        navigation.goBack()
+    const delGo = ()=>{
+        Alert.alert('글 삭제','정말로 삭제하시겠습니까?', [
+            {
+                text:'취소',
+                style:'cancel'
+            },
+
+            {
+                text:'삭제',
+                style:'destructive',
+                onPress:()=>{
+                    deleteReg(data.id)
+                    navigation.goBack()
+                }
+            }
+
+        ])
     }
+
+    const modifyGo = ()=>{
+        navigation.navigate('boardModify',{data, modifyReg})
+    }
+
+    const modifyReg = (updateItem)=>{
+        setDetailData(updateItem)
+        listModifyReg(updateItem)
+    }
+
     return (
-            <View>
-                <Text>수정이지롱</Text>
-                <TextInput value={title} onChangeText={setTitle}/>
-                <TextInput  value={content} onChangeText={setContent}/>
-    
-                <Button title="수정"  onPress={modifyClk} />
-               
+        <View>
+           <Text style={styles.detailTitle}>상세 보기</Text>
+            <Text style={styles.detailttt}>제목</Text>
+            <Text style={styles.detailText}>{detailData.title}</Text>
+            <Text style={styles.detailttt}>내용</Text>
+            <Text style={styles.detailText}>{detailData.content}</Text>
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={[styles.button, styles.modifyButton]} onPress={modifyGo}>
+                    <Text style={styles.buttonText}>수정</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.deleteButton]} onPress={delGo}>
+                    <Text style={styles.buttonText}>삭제</Text>
+                </TouchableOpacity>
             </View>
-        );
+        </View>
+    );
 }
 
-export default boardModify;
+const styles = StyleSheet.create({
+    pageWrap: {
+        flex: 1,
+        backgroundColor: '#FAF8F4', // MAIN 색상
+        padding: 20,
+    },
+    detailTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#333',
+        textAlign: 'center',
+        marginBottom: 50,
+        marginTop:30,
+    },
+    detailttt:{
+        fontSize: 18,
+        fontWeight:'bold',
+        marginBottom:10,
+        marginLeft:10,
+    },
+    detailText: {
+        fontSize: 18,
+        color: '#333',
+        marginBottom: 10,
+        marginLeft:20,
+    },
+    buttonContainer: {
+        flexDirection: 'row', // 버튼을 가로로 배치
+        justifyContent: 'space-between', // 버튼 간 공간을 균등 배치
+        marginTop: 20,
+    },
+    button: {
+        flex: 1, // 버튼들이 같은 비율로 공간을 차지하게 함
+        padding: 15,
+        borderRadius: 8,
+        alignItems: 'center',
+        marginHorizontal: 5, // 버튼 간 간격을 두기 위해 사용
+    },
+    modifyButton: {
+        backgroundColor: '#7A6C64',
+    },
+    deleteButton: {
+        backgroundColor: '#000', // 삭제버튼 색상
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
+
+export default boardDetail;
